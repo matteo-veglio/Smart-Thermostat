e53cc96bb34d4eab34fe6ebaad3a1c12a924adab
Matteo <veglio.matteo@libero.it>
2026-07-27 10:14:31 +0200

    task 7

--- custom_components/smart_thermostat/config_flow.py
+++ custom_components/smart_thermostat/config_flow.py
@@ -1,6 +1,24 @@
+import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow
+from homeassistant.const import CONF_NAME
+from homeassistant.helpers import selector
 
 from .const import DOMAIN
+
+CONF_INDOOR_TEMPERATURE_SENSOR = "indoor_temperature_sensor"
+CONF_INDOOR_HUMIDITY_SENSOR = "indoor_humidity_sensor"
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): selector.TextSelector(),
+        vol.Required(CONF_INDOOR_TEMPERATURE_SENSOR): selector.EntitySelector(
+            selector.EntitySelectorConfig(domain="sensor")
+        ),
+        vol.Optional(CONF_INDOOR_HUMIDITY_SENSOR): selector.EntitySelector(
+            selector.EntitySelectorConfig(domain="sensor")
+        ),
+    }
+)
 
 
 class SmartThermostatConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -8,6 +26,6 @@ class SmartThermostatConfigFlow(ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input: dict | None = None) -> dict:
         if user_input is not None:
-            return self.async_create_entry(title="Smart Thermostat", data={})
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
 
-        return self.async_show_form(step_id="user")
+        return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA)
