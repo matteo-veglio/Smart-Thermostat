5647025f75c5141ad46f6b6a01fd1a93b6fa6d23
Matteo <veglio.matteo@libero.it>
2026-07-27 10:09:44 +0200

    task 5

---
+++ custom_components/smart_thermostat/__init__.py
@@ -0,0 +1,6 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    return True
